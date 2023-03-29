b0df3ed4ccd3cf73e23df2d01b367add05cd6093
= <=>
2023-03-29 18:50:13 +0000

    i have just added link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Unit2OsunRoutes from "./routes/authroutes/Unit2OsunRoutes";
 
 
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "https://codarnetwork.online/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
